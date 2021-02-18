8a01e02c75c0d63ab362f3a4dbcaee5c6300c0d1
annikauniskangas <annikauniskangas@192.168.68.104>
2021-02-18 20:15:14 +0200

    Added two cascading delays to the counter

--- main.s
+++ main.s
@@ -12,7 +12,7 @@ start:
 	movwf	TRISC, A	    ; Move contents of W to TRISC (Access bank)
 	bra 	test		    ; Jump to test
 loop:
-	call	delay
+	call	delay2		    ; Call delay counter
 	movff 	0x06, PORTC	    ; Copy value from 0x06 to PORTC
 	incf 	0x06, W, A	    ; Add 1 to value in 0x06 and put result into W register
 test:
@@ -22,10 +22,18 @@ test:
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
-delay:
-	movlw   0x10
-	movwf   0x20, A
-	decfsz  0x20, F, A
+delay1:
+	movlw   0x10		    ; Put value 0x10 into W
+	movwf   0x20, A		    ; Move value in W to file register address 0x20
+	decfsz  0x20, F, A	    ; Decrement value in 0x20. If 0, skip next line
+	bra	$-2
+	return
+	
+delay2:
+	call	delay1		    ; Call counter delay1
+	movlw	0x20		    ; Put the value 0x20 into W
+	movwf	0x30		    ; Move value in W to file register address 0x30
+	decfsz	0x30,F,A	    ; Decrement value in 0x30. If 0, skip next line
 	bra	$-2
 	return
 	
