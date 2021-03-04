dca1706f7519878700a8c6a1fca0faf9f2500155
annikauniskangas <annikauniskangas@anni-mbp>
2021-03-04 13:00:41 +0000

    Program that sends byte patterns via a serial link to an external shift register

--- main.s
+++ main.s
@@ -1,60 +1,32 @@
 	#include <xc.inc>
-	
+; This code sends byte patterns to an external shift register, using SPI as a "serial link"
 psect	code, abs
 main:
 	org 0x0
-	goto	setup
+	goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-	; ******* Programme FLASH read Setup Code ****  
-setup:	
-	bcf	CFGS		    ; point to Flash program memory  
-	bsf	EEPGD		    ; access Flash program memory
-	movlw 	0x0		    ; Put 0 to W register
-	movwf	TRISC, A	    ; Move contents of W to TRISC (to make it an output)
-	movlw	0b11111111	    ; Put binary 11111111 to W register
-	movwf	TRISD		    ; Move contents of W to TRISD (to make it an input)
-	goto	start
-	; ******* My data and where to put it in RAM *
-myTable:
-	db	0b11110000, 0b00001111, 0b01010101, 0b10101010,0b10010110
-	myArray EQU 0x400	    ; Address in RAM for data
-	counter EQU 0x10	    ; Address of counter variable
-	align	2		    ; ensure alignment of subsequent instructions 
-	; ******* Main programme *********************
-start:	
-   
-	lfsr	0, myArray	    ; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A	    ; load upper bits to TBLPTRU
-	movlw	high(myTable)	    ; address of data in PM
-	movwf	TBLPTRH, A	    ; load high byte to TBLPTRH
-	movlw	low(myTable)	    ; address of data in PM
-	movwf	TBLPTRL, A	    ; load low byte to TBLPTRL
-	movlw	5		    ; 22 bytes to read
-	movwf 	counter, A	    ; our counter register
-loop:
-	nop			    ; This is to switch program on/off with PORTD pin3
-	btfsc	PORTD, 3	    ; If PORTD pin 3 is 0, skip next line
-	bra	$-3
-    
-        tblrd*+			    ; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, PORTC	    ; move read data from TABLAT to PORTC	
+	; ******* Programme Setup Subroutine *****  
+SPI_MasterInit:
 	
-	btfsc	PORTD, 0	    ; If PORTD pin 0 is 0, skip next line
-	call	delay2		    ; These will adjust delay length based on PORTD input
+	clrf	PORTD
+	clrf	TRISD
+	Banksel	SSP2STAT
+	BCF	CKE		    ; CKE bit in SSP2STAT
+	movlb	0x00
+	; MSSP enable; CKP=1, SPI master, clock=Fosc/64 (=1MHz)
+	movlw	(SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
+	movwf	SSP2CON1, A
+	; SDO2 output; SCK2 output
+	BCF	TRISD, PORTD_SDO2_POSN, A ; SDO2 output
+	BCF	TRISD, PORTD_SCK2_POSN, A ; SCK2 output
+	BSF	PORTD, 0, A	    ; Set master reset (off)
 	
-	btfsc	PORTD, 1		    
-	call	delay3
+	return
 	
-	btfsc	PORTD, 2
-	call	delay4
 	
-	decfsz	counter, A	    ; count down to zero
-	bra	loop		    ; keep going until finished
-	goto	0x00		    ; Re-run program
-	
+	; ******* Delay subroutines ******
 delay1:
 	movlw   0xFF		    ; Put value 0x10 into W
 	movwf   0x20, A		    ; Move value in W to file register address 0x20
@@ -78,13 +50,54 @@ delay3:
 	bra	$-6
 	return
 	
-delay4:
-	movlw	0x10
-	movwf	0x50
-	call	delay3
-	decfsz	0x50, F, A
-	bra	$-6
+	
+	; ******* Serial data transmission subroutines *****
+SPI_MasterTransmit:		    ; Start transmission of data (held in W)
+	movwf	SSP2BUF, A	    ; Write data to output buffer
 	return
 	
+Wait_Transmit:			    ; Wait for transmission to be complete
+	btfss	SSP2IF		    ; Check interrupt flag to see if data has been sent
+	bra	Wait_Transmit
+	BCF	SSP2IF		    ; Clear interrupt flag
+	return
+
+
+	; ******* Main programme *******
+start:
+	call	SPI_MasterInit	    ; Setup SPI
+	movlw	0b11111111
+	call	SPI_MasterTransmit
+	call	Wait_Transmit
+	call	delay3
+	BCF	PORTD, 0, A	    ; Clear master reset (on)
+	call	delay1		    ; Shift register has been reset
+	BSF	PORTD, 0, A	    ; Set master reset (off)
+	
+	movlw	0b10101010
+	call	SPI_MasterTransmit
+	call	Wait_Transmit
+	call	delay3
+	BCF	PORTD, 0, A	    ; Clear master reset (on)
+	call	delay1		    ; Shift register has been reset
+	BSF	PORTD, 0, A	    ; Set master reset (off)
+	
+	movlw	0b10001000
+	call	SPI_MasterTransmit
+	call	Wait_Transmit
+	call	delay3
+	BCF	PORTD, 0, A	    ; Clear master reset (on)
+	call	delay1		    ; Shift register has been reset
+	BSF	PORTD, 0, A	    ; Set master reset (off)
+	
+	goto	0
+	
+	 
+	
+	
+	
+
+
+
 
 	end	main
